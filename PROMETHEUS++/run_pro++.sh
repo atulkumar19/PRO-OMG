e90b62e25c31cdb9d371da48b7598cd31b9dd236
L Carbajal <leopoldo.carbajal@correo.nucleares.unam.mx>
2019-04-08 15:01:04 -0500

    ADDED: bash file to run PRO++ when the external libraries' path is not in the LD_LIBRARY_PATH env variable

---
+++ PROMETHEUS++/run_pro++.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Paths to external libraries
+HDF5_PATH="/home/leo/Documents/PRO++/HDF5/lib"
+ARMA_PATH="/home/leo/Documents/PRO++/arma_libs/usr/lib64"
+
+# Number of MPI processes
+NUM_MPI_PROCESSES=2
+
+# Location of outputs folder
+LOC_OUTPUT_FOLDER="/home/leo/Documents/PRO++/PROMETHEUS++/outputFiles"
+
+# File identifier
+FILE_ID="warm_plasma"
+
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${HDF5_PATH}:${ARMA_PATH}
+
+mpirun -x LD_LIBRARY_PATH -np $((NUM_MPI_PROCESSES))  bin/PROMETHEUS++ ${LOC_OUTPUT_FOLDER} ${FILE_ID}

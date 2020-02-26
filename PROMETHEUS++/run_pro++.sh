387f3b1c34723a7e330ab0ffa42d7cc61cf9433e
L Carbajal <leopoldo.carbajal@correo.nucleares.unam.mx>
2020-02-26 14:37:45 -0600

    Commit before merging to master

--- PROMETHEUS++/run_pro++.sh
+++ PROMETHEUS++/run_pro++.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
 
+# Path to installation folder
+# Change this folder to your installation folder of PROMETHEUS++
+ROOT="/home/leo/Documents/PRO++"
+
+
 # Paths to external libraries
-HDF5_PATH="/home/leo/Documents/PRO++/HDF5/lib"
-ARMA_PATH="/home/leo/Documents/PRO++/arma_libs/usr/lib64"
-# ARMA_PATH="/home/leo/Documents/PRO++/arma_libs_9/lib"
+HDF5_PATH=${ROOT}"/HDF5/lib"
+ARMA_PATH=${ROOT}"/arma_libs/usr/lib64"
 
 # Number of MPI processes
 NUM_MPI_PROCESSES=2
 
 # Location of outputs folder
-# LOC_OUTPUT_FOLDER="/home/leo/Documents/PRO++/PROMETHEUS++/Tests"
-LOC_OUTPUT_FOLDER="/home/leo/Documents/PRO++/PROMETHEUS++/outputFiles"
+LOC_OUTPUT_FOLDER=${ROOT}"/PROMETHEUS++/outputFiles"
 
-# File identifier
+# Identifier for input file. If none used, default input files used.
 # FILE_ID="dispersion_relation"
 # FILE_ID="GC"
 # FILE_ID="warm_plasma"
-FILE_ID="test2"
 
 rm -r ${LOC_OUTPUT_FOLDER}"/"${FILE_ID}
 
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${HDF5_PATH}:${ARMA_PATH}
 
-mpirun -x LD_LIBRARY_PATH -x OMP_NUM_THREADS=2 -np $((NUM_MPI_PROCESSES))  bin/PROMETHEUS++ ${LOC_OUTPUT_FOLDER} ${FILE_ID}
-#mpirun -x LD_LIBRARY_PATH -x OMP_NUM_THREADS=2 -np $((NUM_MPI_PROCESSES))  bin/PROMETHEUS++ ${LOC_OUTPUT_FOLDER}
+# mpirun -x LD_LIBRARY_PATH -x OMP_NUM_THREADS=2 -np $((NUM_MPI_PROCESSES))  bin/PROMETHEUS++ ${LOC_OUTPUT_FOLDER} ${FILE_ID}
+mpirun -x LD_LIBRARY_PATH -x OMP_NUM_THREADS=2 -np $((NUM_MPI_PROCESSES))  bin/PROMETHEUS++ ${LOC_OUTPUT_FOLDER}
